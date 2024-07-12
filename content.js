55b457e1b321b429ee6bbe51d3adb7bc1ce42a3f
LuongTD <luongtd@kaopiz.com>
2024-07-12 13:03:30 +0700

    update

--- content.js
+++ content.js
@@ -12,31 +12,31 @@ function hideTranslateIcon(id) {
 }
 
 function showTranslateIcon(x, y, text) {
-    console.log("🚀 ~ showTranslateIcon ~ text:", text)
-    // let translateIcon = document.getElementById('translateIcon');
-    // if (!translateIcon) {
-        const id = new Date().getTime();
-        let translateIcon = document.createElement('a');
-        translateIcon.id = id;
-        translateIcon.style.position = 'absolute';
-        translateIcon.style.cursor = 'pointer';
-        translateIcon.target = '_blank';
-        document.body.appendChild(translateIcon);
+    const id = new Date().getTime();
+    let translateIcon = document.createElement('a');
+    translateIcon.id = id;
+    translateIcon.style.position = 'absolute';
+    translateIcon.style.cursor = 'pointer';
+    translateIcon.target = '_blank';
+    document.body.appendChild(translateIcon);
 
-        const img = document.createElement('img');
-        //   img.src = chrome.runtime.getURL('icon.png');
-        img.src = "https://i.pinimg.com/originals/e5/b5/f7/e5b5f75aee252932abc227fc80cbb633.jpg";
-
-        img.style.width = '20px';
-        img.style.height = '20px';
-        translateIcon.appendChild(img);
-    // }
+    const img = document.createElement('img');
+    img.src = "https://i.pinimg.com/originals/e5/b5/f7/e5b5f75aee252932abc227fc80cbb633.jpg";
+    img.style.width = '20px';
+    img.style.height = '20px';
+    translateIcon.appendChild(img);
 
     // Set href dynamically based on the selected text and stored language
     chrome.storage.sync.get('selectedLanguage', (data) => {
         const language = data.selectedLanguage || 'en';
-        const url = `https://translate.google.com/?sl=auto&tl=${language}&text=${encodeURIComponent(text)}&op=translate`;
-        translateIcon.href = url;
+        chrome.storage.sync.get('selectedTypeTranslate', (data) => {
+            const typeTranslate = data.selectedTypeTranslate || 'chatGPT';
+            if (typeTranslate === 'chatGPT') {
+                translateIcon.href = `https://chatgpt.com/chat?model=gpt-3.5-turbo-0613&prompt=${encodeURIComponent(text)}`;
+            } else {
+                translateIcon.href = `https://translate.google.com/?sl=auto&tl=${language}&text=${encodeURIComponent(text)}&op=translate`;
+            }
+        });
     });
 
     translateIcon.style.top = `${y}px`;
--- popup.js
+++ popup.js
@@ -11,23 +11,26 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   });
 
-    // Load the selected language from storage
-    chrome.storage.sync.get('toolTranslate', (data) => {
-      console.log("🚀 ~ chrome.storage.sync.get ~ data:", data)
-      if (data.toolTranslate) {
-        toolTranslate.value = data.toolTranslate;
-        localStorage.setItem('toolTranslate', data.toolTranslate);
-      }
-    });
+  // Load the selected language from storage
+  chrome.storage.sync.get('selectedTypeTranslate', (data) => {
+    if (data.selectedTypeTranslate) {
+      toolTranslate.value = data.selectedTypeTranslate;
+      localStorage.setItem('selectedTypeTranslate', data.selectedTypeTranslate);
+    }
+  });
 
   // Save the selected language to storage when the button is clicked
   saveButton.addEventListener('click', () => {
     const selectedLanguage = languageSelect.value;
+    const selectedTypeTranslate = toolTranslate.value;
+
     chrome.storage.sync.set({ selectedLanguage }, () => {
       localStorage.setItem('selectedLanguage', selectedLanguage);
     });
-    chrome.storage.sync.set({ toolTranslate }, () => {
-      localStorage.setItem('toolTranslate', toolTranslate);
+    chrome.storage.sync.set({ selectedTypeTranslate }, () => {
+      localStorage.setItem('selectedTypeTranslate', selectedTypeTranslate);
     });
+
+    alert('Language and type of translation saved successfully!');
   });
 });
